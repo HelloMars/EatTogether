6d1e5852c0b9ec0fbf2622e10cbf7733ecb7d828
zhumeng <zhumeng@rd.netease.com>
2015-02-03 19:56:06 +0800

    ADD: angular.js

---
+++ public/js/eat.js
@@ -0,0 +1,7 @@
+(function(){
+    var module = angular.module("eat",[]);
+    module.run(function() {
+        AV.initialize("vk84p7j0sizwl03zgvb3y1eg6z7klbs97hrgock7ilfascaf",
+            "pxbvfffu8uli2tcld6sg9pgfouoq1fbse6l4bf0xt1ukaqrq");
+    });
+})();
